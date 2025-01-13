7125774c686452c2e0c8f2c38ad2b3fe2429884d
TajahH <167078781+TajahH@users.noreply.github.com>
2025-01-13 14:57:56 +0000

    TODO implement conversion logic

--- primer1/primer1.js
+++ primer1/primer1.js
@@ -1,14 +1,41 @@
 function temperatureConversion(temperature, fromScale, toScale){
-
 // TODO: Validate the input:
 //        - Check if the temperature is null, undefined, or a non-numeric string. If so, throw an error "Invalid temperature input".
 //        - Convert the temperature to a number.
 //        - Normalise fromScale and toScale to uppercase.
 //        - Check if fromScale and toScale are valid ( e.g. 'C', 'F'). If not, throw an error "Invalid conversion type or input scale".
 
+    parseFloat(temperature);
+
+    if (temperature == null | undefined | NaN) {
+        throw new Error("Invalid temperature input")
+    } else 
+
+    fromScale.toUpperCase;
+    toScale.toUpperCase;
+
+    if (fromScale && toScale != "C" | "F" | "c" | "f"){
+        throw new Error("Invalid conversion type or input scale")
+    } else if (fromScale == "C"){
+
+    }
+
+
 // TODO: Define helper functions for the conversions:
 //        - toFahrenheit(celsius): Converts Celsius to Fahrenheit.
 //        - toCelsius(fahrenheit): Converts Fahrenheit to Celsius.
+
+    function toFahrenheit(celsius){
+        let fahrenheit = (celsius * 9 / 5) + 32
+        return fahrenheit 
+    }    
+
+    function toCelsius(fahrenheit){
+        let celsius = (fahrenheit - 32) * 5 / 9
+        return celsius
+    }
+
+    
 //        
 // TODO: Implement the conversion logic:
 //        - Use conditional logic to handle different toScale values (e.g. 'C', 'F').
